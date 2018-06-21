6816fa9d26044f7910738329c7c066b4ecd32514
Hatanaka Yamato <kbc16a17@stu.kawahara.ac.jp>
2018-06-21 10:29:30 +0900

    ネットワーク対戦シーンの追加

---
+++ Assets/NotTetrin/Scripts/Ingame/Multi/GameManager.cs
@@ -0,0 +1,158 @@
+using System;
+using System.Threading.Tasks;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Events;
+using NotTetrin.SceneManagement;
+
+using Random = UnityEngine.Random;
+
+namespace NotTetrin.Ingame.Multi {
+    [RequireComponent(typeof(AudioSource), typeof(PhotonView))]
+    public class GameManager : MonoBehaviour {
+        [SerializeField] private Director director;
+        [SerializeField] private IngameAudioManager audioManager;
+        [SerializeField] private MinoManager minoManager;
+        [SerializeField] private AudioClip[] bgmClips;
+
+        public UnityEvent OnRoundStart;
+        public UnityEvent OnRoundEnd;
+
+        private PhotonView photonView;
+        private AudioSource bgmAudioSource;
+
+        public PlayerSide PlayerSide => (PhotonNetwork.player.ID == 1) ? PlayerSide.Left : PlayerSide.Right;
+
+        private double gameOverTime = 0.0;
+        private bool accepted = false;
+        private bool hitMino = false;
+        private bool hitOpponent = false;
+
+        private void Awake() {
+            photonView = GetComponent<PhotonView>();
+            bgmAudioSource = GetComponent<AudioSource>();
+        }
+
+        private void Start() {
+            minoManager.HitMino += onHitMino;
+            ready();
+        }
+
+        private void Update() {
+            if (Input.GetButtonDown(@"Escape")) {
+                if (PhotonNetwork.connected) { PhotonNetwork.Disconnect(); }
+                SceneTransit.Instance.LoadScene(@"Title", 0.4f);
+            }
+        }
+
+        private void reset() {
+            accepted = false;
+            audioManager.Stop(IngameSfxType.GameOver);
+            minoManager.Reset();
+        }
+
+        private void ready() {
+            photonView.RPC(@"OnReadyOpponent", PhotonTargets.Others);
+        }
+
+        private void gamestart() {
+            reset();
+            OnRoundStart.Invoke();
+
+            var clipIndex = Random.Range(0, bgmClips.Length - 1);
+            bgmAudioSource.clip = bgmClips[clipIndex];
+            bgmAudioSource.Play();
+
+            audioManager.Play(IngameSfxType.GameStart);
+            minoManager.Next();
+        }
+
+        private void gameover() {
+            gameOverTime = PhotonNetwork.time;
+            photonView.RPC(@"OnGameoverOpponent", PhotonTargets.Others, gameOverTime);
+        }
+
+        private void onHitMino(object sender, EventArgs args) {
+            // 天井に当たったらゲームオーバー
+            if (director.Ceiling.IsHit) {
+                minoManager.Release();
+                gameover();
+            } else {
+                /* ターン制 /
+                photonView.RPC(@"OnOpponentHit", PhotonTargets.Others);
+                if (hitOpponent) {
+                    minoManager.Next();
+                    hitOpponent = false;
+                } else {
+                    minoManager.Release();
+                    hitMino = true;
+                }
+                /*/
+                minoManager.Next();
+                //*/
+            }
+        }
+
+        public void OnPhotonPlayerDisconnected(PhotonPlayer player) {
+            Debug.Log($"disconnected opponent.");
+        }
+
+        /* ターン制 /
+        [PunRPC]
+        private void OnOpponentHit() {
+            if (hitMino) {
+                minoManager.Next();
+                hitMino = false;
+            } else {
+                hitOpponent = true;
+            }
+        }
+        //*/
+
+        [PunRPC]
+        private void OnReadyOpponent() {
+            gamestart();
+        }
+
+        [PunRPC]
+        private void OnGameoverOpponent(double timestamp) {
+            Debug.Log(@"OnGameoverOpponent (" + timestamp + ")");
+            minoManager.Destroy();
+
+            if (timestamp < gameOverTime) {
+                photonView.RPC(@"OnWinAccepted", PhotonTargets.Others);
+                OnLoseAccepted();
+            } else {
+                photonView.RPC(@"OnLoseAccepted", PhotonTargets.Others);
+                OnWinAccepted();
+            }
+        }
+
+        [PunRPC]
+        private void OnWinAccepted() {
+            if (accepted) { return; }
+            Debug.Log($"you win.");
+
+            bgmAudioSource.Stop();
+            audioManager.Play(IngameSfxType.GameOver);
+            Invoke("ready", 9.0f);
+
+            accepted = true;
+        }
+
+        [PunRPC]
+        private void OnLoseAccepted() {
+            if (accepted) { return; }
+            Debug.Log($"you lose.");
+
+            bgmAudioSource.Stop();
+            OnRoundEnd.Invoke();
+            audioManager.Play(IngameSfxType.GameOver);
+            Invoke("ready", 9.0f);
+
+            accepted = true;
+        }
+    }
+}
---
+++ Assets/NotTetrin/Scripts/Ingame/Multi/NetworkInstantiator.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+namespace NotTetrin.Ingame.Multi {
+    public class NetworkInstantiator : Instantiator {
+        public override GameObject Instantiate(GameObject original, Vector3 position, Quaternion rotation, byte group) => PhotonNetwork.Instantiate(original.name, position, rotation, group);
+
+        public override void Destroy(GameObject obj) {
+            var view = obj.GetComponent<PhotonView>();
+            if (view) {
+                PhotonNetwork.Destroy(view);
+            } else {
+                Object.Destroy(obj);
+            }
+        }
+    }
+}
