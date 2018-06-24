e76df3919002126feacbe870d33a1fb83f3c6b27
Hatanaka Yamato <kbc16a17@stu.kawahara.ac.jp>
2018-06-24 23:52:10 +0900

    タイトルでボタンが複数回押せてしまう不具合を修正

---
+++ Assets/NotTetrin/Scripts/UI/SelectableToggler.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace NotTetrin.UI { 
+    public class SelectableToggler : MonoBehaviour {
+        [SerializeField]
+        private Selectable[] objects;
+
+        public void ToggleAll() {
+            foreach (var obj in objects) {
+                obj.interactable = !obj.interactable;
+            }
+        }
+    }
+}
