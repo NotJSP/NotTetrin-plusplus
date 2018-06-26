22895656aca739889cb119cbf5b5b854ea3a45f4
Hatanaka Yamato <kbc16a17@stu.kawahara.ac.jp>
2018-06-26 14:27:38 +0900

    密度インジケータの準備

--- Assets/NotTetrin/Scripts/Ingame/Single/Marathon/CreateTileAndGrouping.cs
+++ Assets/NotTetrin/Scripts/Ingame/Single/Marathon/CreateTileAndGrouping.cs
@@ -6,45 +6,41 @@ using UnityEngine.UI;
 
 namespace NotTetrin.Ingame.Single.Marathon {
     [DefaultExecutionOrder(1)]
-    [RequireComponent(typeof(CreateTile))]
+    [RequireComponent(typeof(Renderer), typeof(CreateTile))]
     public class CreateTileAndGrouping : MonoBehaviour {
         [SerializeField]
-        private Text text;
-
-        private List<GameObject> minos = new List<GameObject>();
+        private DensityIndicator indicator;
 
         private new Renderer renderer;
+        private List<GameObject> minos = new List<GameObject>();
+        private ColliderGroup group;
 
-        public ColliderGroup group;
-
-        private bool minomoving;
+        public bool IsEntered => group.EnteredAll;
 
         private void Awake() {
             renderer = GetComponent<Renderer>();
 
             // 作ったタイルの子供達のColliderHelperを取得して配列にする
             var colliders = GetComponent<CreateTile>().children
-                .Select(o => o.GetComponent<ColliderHelper>())
-                .ToArray();
+                .Select(o => o.GetComponent<ColliderHelper>());
             group = new ColliderGroup(colliders);
         }
 
         public void Update() {
-            renderer.enabled = group.EnteredAll;
+            // var density = (float)group.EnterCount / group.Children.Length;
+            // indicator.UpdateDensity(density);
+            renderer.enabled = IsEntered;
         }
 
-
         public void DeleteMino() {
-            if (group.EnteredAll) {
-                Debug.Log("ミノ削除");
+            Debug.Log("ミノ削除");
 
-                while (minos.Count > 0) {
-                    Debug.Log(minos[0] + "削除");
-                    minos[0].transform.Translate(10000, 10000, 10000);
-                    Destroy(minos[0], 1.0f); //即消しだと判定が残るから、時間差攻撃。
-                    minos.Remove(minos[0]);
-                }
+            foreach (var mino in minos) {
+                Debug.Log(mino + "削除");
+                mino.transform.Translate(10000, 10000, 10000);  // OnTriggerExit2Dを呼び出すため範囲外へ移動
+                Destroy(mino, 1.0f);    // 即消しだと判定が残るから時間差攻撃
             }
+            minos.Clear();
         }
 
         private void OnTriggerEnter2D(Collider2D collision) {
