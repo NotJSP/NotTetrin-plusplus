2d3ae357d8e098a042f315665dfd9d6c3d0dded3
Hatanaka Yamato <kbc16a17@stu.kawahara.ac.jp>
2018-07-01 02:28:51 +0900

    プレイヤー名を保持するよう変更

--- Assets/NotTetrin/Scripts/Title/TitleManager.cs
+++ Assets/NotTetrin/Scripts/Title/TitleManager.cs
@@ -1,10 +1,21 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using NotTetrin.Constants;
 
 namespace NotTetrin.Title {
     public class TitleManager : MonoBehaviour {
-        public static readonly float TransitionDuration = 0.4f;
+        [SerializeField] private InputField nameField;
+
+        public static readonly float TransitionDuration = 1.0f;
+
+        private void Awake() {
+            // TODO: 本番では保存しない
+            if (PlayerPrefs.HasKey(PlayerPrefsKey.PlayerName)) {
+                nameField.text = PlayerPrefs.GetString(PlayerPrefsKey.PlayerName);
+            }
+        }
 
         private void Update() {
             if (Input.GetKey(KeyCode.LeftControl) && Input.GetKeyDown(KeyCode.D)) {
