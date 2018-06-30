6aa4b0d3d37310c7775186b1bb9c948fb2554d29
Hatanaka Yamato <kbc16a17@stu.kawahara.ac.jp>
2018-07-01 02:20:38 +0900

    SingletonMonoBehaviourの挙動を一部変更

--- Assets/NotTetrin/Scripts/Core/SingletonMonoBehaviour.cs
+++ Assets/NotTetrin/Scripts/Core/SingletonMonoBehaviour.cs
@@ -8,15 +8,18 @@ namespace NotTetrin {
 
         public static T Instance {
             get {
-                if (instance == null) {
-                    instance = FindObjectOfType<T>();
+                if (instance) { return instance; }
 
-                    if (instance == null) {
-                        Debug.LogError(typeof(T) + " is nothing");
-                    }
-                }
+                // Hierarchyから探す
+                instance = FindObjectOfType<T>();
+                if (instance) { return instance; }
 
-                return instance;
+                // Resourcesから探す
+                var resource = Resources.Load<T>(typeof(T).Name);
+                instance = Instantiate(resource);
+                if (instance) { return instance; }
+
+                return null;
             }
         }
 
