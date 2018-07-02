4057e7c931d74b7462e5563be009d5f8bfec81f3
Hatanaka Yamato <kbc16a17@stu.kawahara.ac.jp>
2018-07-02 15:02:24 +0900

    ビルド時にバージョンを上げるよう変更

---
+++ Assets/NotTetrin/Editor/VersionInfo.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using UnityEditor.Callbacks;
+
+public class VersionInfo : MonoBehaviour {
+    [PostProcessBuild]
+    public static void OnPostprocessBuild(BuildTarget target, string pathToBuiltProject) {
+        float versionFloat;
+
+        if (float.TryParse(PlayerSettings.bundleVersion, out versionFloat)) {
+            versionFloat += 0.01f;
+            PlayerSettings.bundleVersion = versionFloat.ToString();
+        }
+    }
+}
