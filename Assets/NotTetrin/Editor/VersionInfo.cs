118743c3d5998acfe11559d767a16072be920a77
Hatanaka Yamato <kbc16a17@stu.kawahara.ac.jp>
2018-07-03 11:22:32 +0900

    バージョンの表記を変更 (X.X => X.X.X#YY)

--- Assets/NotTetrin/Editor/VersionInfo.cs
+++ Assets/NotTetrin/Editor/VersionInfo.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor;
@@ -7,11 +8,16 @@ using UnityEditor.Callbacks;
 public class VersionInfo : MonoBehaviour {
     [PostProcessBuild]
     public static void OnPostprocessBuild(BuildTarget target, string pathToBuiltProject) {
-        float versionFloat;
-
-        if (float.TryParse(PlayerSettings.bundleVersion, out versionFloat)) {
-            versionFloat += 0.01f;
-            PlayerSettings.bundleVersion = versionFloat.ToString();
+        var appVersion = PlayerSettings.bundleVersion;
+        var versions = appVersion.Split('#');
+        var version = versions[0];
+        var buildNumberString = versions[1];
+        int buildNumber;
+        if (int.TryParse(buildNumberString, out buildNumber)) {
+            buildNumber++;
+            PlayerSettings.bundleVersion = version + '#' + buildNumber.ToString();
+        } else {
+            throw new SystemException(@"バージョンの書式が不正です。");
         }
     }
 }
