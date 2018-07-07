e58e3398adf102e1bc8d310d87cf185a4a935d9f
Hatanaka Yamato <kbc16a17@stu.kawahara.ac.jp>
2018-07-08 01:21:09 +0900

    シーン遷移しないことがある不具合を修正

--- Assets/NotTetrin/Scripts/SceneManagement/LoadingScreen.cs
+++ Assets/NotTetrin/Scripts/SceneManagement/LoadingScreen.cs
@@ -1,12 +1,6 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections;
 using UnityEngine;
 using UnityEngine.UI;
-using UnityEngine.SceneManagement;
 
 namespace NotTetrin.SceneManagement {
     [RequireComponent(typeof(CanvasGroup), typeof(FadeImage))]
--- Assets/NotTetrin/Scripts/SceneManagement/SceneController.cs
+++ Assets/NotTetrin/Scripts/SceneManagement/SceneController.cs
@@ -1,15 +1,18 @@
 ﻿using System;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 using System.Collections;
 using System.Collections.Generic;
 
 namespace NotTetrin.SceneManagement {
     public class SceneController : SingletonMonoBehaviour<SceneController> {
-        [SerializeField] private Canvas canvas;
-        [SerializeField] private LoadingScreen screen;
+        [SerializeField] Canvas canvas;
+        [SerializeField] CanvasGroup canvasGroup;
+        [SerializeField] LoadingScreen screen;
 
         public event EventHandler SceneReady;
+        private string lastSceneName;
         private Coroutine coroutine;
 
         private void Start() {
@@ -17,7 +20,13 @@ namespace NotTetrin.SceneManagement {
         }
 
         public void LoadScene(string scene, float interval) {
-            if (coroutine != null) { return; }
+            if (lastSceneName == scene) { return; }
+            StartCoroutine(waitAndTransitScene(scene, interval));
+            lastSceneName = scene;
+        }
+
+        private IEnumerator waitAndTransitScene(string scene, float interval) {
+            yield return new WaitUntil(() => coroutine == null);
             coroutine = StartCoroutine(transitScene(scene, interval));
         }
 
@@ -27,8 +36,10 @@ namespace NotTetrin.SceneManagement {
             operation.allowSceneActivation = false;
 
             // フェードイン
+            canvasGroup.blocksRaycasts = true;
             canvas.enabled = true;
             screen.FadeIn(interval);
+
             // ロードアニメーション
             screen.Play(operation);
             yield return new WaitWhile(() => screen.isFading);
@@ -44,8 +55,11 @@ namespace NotTetrin.SceneManagement {
             // フェードアウト
             screen.FadeOut(interval);
             yield return new WaitWhile(() => screen.isFading);
+
+            // 後処理
             screen.Stop();
             canvas.enabled = false;
+            canvasGroup.blocksRaycasts = false;
 
             // コルーチンの削除
             coroutine = null;
