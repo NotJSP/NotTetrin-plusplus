c9e6ba834720d1a3ad5f5e489367608a9842d01c
Hatanaka Yamato <kbc16a17@stu.kawahara.ac.jp>
2018-07-12 23:26:38 +0900

    密度インジケーターの調整

--- Assets/NotTetrin/Scripts/Ingame/Marathon/DensityIndicator.cs
+++ Assets/NotTetrin/Scripts/Ingame/Marathon/DensityIndicator.cs
@@ -5,26 +5,42 @@ namespace NotTetrin.Ingame.Marathon {
     [RequireComponent(typeof(SpriteRenderer))]
     public class DensityIndicator : MonoBehaviour {
         private new SpriteRenderer renderer;
-        private Vector3 localScale;
         private float maxScaleX;
+        private float halfSizeX;
+        private Vector3 defaultPosition;
+        private Vector3 scale;
+        private Color color;
 
         public void Initialize(GameObject wall) {
-            this.localScale = transform.localScale;
-            this.renderer = GetComponent<SpriteRenderer>();
+            renderer = GetComponent<SpriteRenderer>();
 
             var rateX = wall.size().x / gameObject.size().x;
-            maxScaleX = localScale.x * rateX;
+            maxScaleX = transform.localScale.x * rateX;
 
-            transform.position = new Vector3(wall.transform.position.x, transform.position.y);
-            transform.localScale = new Vector3(0, localScale.y, localScale.z);
+            scale = new Vector3(maxScaleX, transform.localScale.y);
+            transform.localScale = scale;
+            halfSizeX = 0.5f * gameObject.size().x;
+
+            scale.x = 0.0f;
+            transform.localScale = scale;
+
+            defaultPosition = new Vector3(wall.transform.position.x - halfSizeX, transform.position.y);
+            color = new Color(renderer.color.r, renderer.color.g, renderer.color.b, 0.0f);
+
+            transform.position = defaultPosition;
+            renderer.color = color;
         }
 
         public void UpdateDensity(float density) {
-            var density_pow = Mathf.Pow(density, 2.5f);
-            transform.localScale = new Vector3(maxScaleX * density_pow, localScale.y, localScale.z);
+            var density_pow = Mathf.Pow(density, 2.0f);
 
-            var color = renderer.color;
-            color.a = density_pow;
+            var position = new Vector3(defaultPosition.x + density_pow * halfSizeX, defaultPosition.y);
+            transform.position = position;
+
+            scale.x = maxScaleX * density_pow;
+            transform.localScale = scale;
+
+            color.a = Mathf.Pow(density_pow, 2.5f);
             renderer.color = color;
         }
     }
